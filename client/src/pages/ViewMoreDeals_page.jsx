6eb128b5ba23df3a3eb077f714e446ec4443b369
KD-43 <duongtkevin5438@gmail.com>
2026-01-26 16:35:08 -0800

    chore: update view more page to match newer ui style

--- client/src/pages/ViewMoreDeals_page.jsx
+++ client/src/pages/ViewMoreDeals_page.jsx
@@ -42,12 +42,21 @@ export default function ViewMoreDeals () {
                 <Navbar />
                 <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', flexGrow: 1, justifyContent: 'space-between', }}>
                     <Grid container spacing={1} sx={{ justifyContent: 'center', alignItems: 'center', mt: 4, }}>
-                        <Grid size={6} sx={{ justifyItems: 'end', }}>
-                            <Typography variant={'h6'}>Viewing more:</Typography>
+                        <Grid container spacing={1} size={8} sx={{ color: 'secondary.main', }}>
+                            
+                            <Grid size={12} sx={{ display: 'flex', justifyContent: 'center', }}>
+                                <Typography variant={'h5'} fontWeight={'900'}>Viewing more:</Typography>
+                                <Typography variant={'h5'} fontWeight={'900'} sx={{ ml: 2 }}>"{dealsType ? dealsType[0].toUpperCase() + dealsType.slice(1) : ''}"</Typography>
+                            </Grid>
                         </Grid>
-                        <Grid size={6}>
-                            <Typography variant={'h6'}>{dealsType ? dealsType[0].toUpperCase() + dealsType.slice(1) : ''}</Typography>
-                        </Grid>
+
+                        <Box sx={{ backgroundColor: theme.palette.tertiary.main, borderRadius: 40, height: 48, width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', px: 3, py: 2, mt: 5, }}>
+                            <Grid container spacing={1} sx={{ justifyContent: 'space-between'}}>
+                                <Grid size={2} sx={{ display: 'grid', placeItems: 'start'}}><Typography variant="body1" fontWeight={'900'}>TITLE</Typography></Grid>
+                                <Grid size={7} sx={{ display: 'grid', placeItems: 'center'}}><Typography variant="body1" fontWeight={'900'}>STORE</Typography></Grid>
+                                <Grid size={2} sx={{ display: 'grid', placeItems: 'center'}}><Typography variant="body1" fontWeight={'900'}>PRICE | DISCOUNT</Typography></Grid>
+                            </Grid>
+                        </Box>
                     </Grid>
 
                     <List
@@ -66,7 +75,7 @@ export default function ViewMoreDeals () {
 
                     <Grid container spacing={8} sx={{ my: 20, display: 'flex', }}>
                         <Grid size={6}>
-                            <Typography variant="h3" fontWeight={'700'} color={'primary'} textAlign={'end'}>
+                            <Typography variant="h3" fontWeight={'900'} color={'primary'} textAlign={'end'}>
                                 Have a title in mind? <br /> Look for it!
                             </Typography>
                         </Grid>
