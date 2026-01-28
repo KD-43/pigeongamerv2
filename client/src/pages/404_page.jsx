2c6357180e0d448d5808d8f9324cfc3091496878
KD-43 <duongtkevin5438@gmail.com>
2026-01-27 22:04:30 -0800

    update: match error page to overall app design

--- client/src/pages/404_page.jsx
+++ client/src/pages/404_page.jsx
@@ -1,7 +1,22 @@
+import { useNavigate } from "react-router";
+import Navbar from "../components/Navbar";
+import { Box, Container, Typography, useTheme } from '@mui/material';
+import { ErrorOutline } from "@mui/icons-material";
+import Footer from "../components/FooterComponent";
+
 export default function ErrorPage () {
+    const theme = useTheme();
+
     return (
         <>
-            <div>This is the catchall error page.</div>
+            <Container sx={{ paddingTop: '56px', height: '100vh', }}>
+                <Navbar />
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%', color: 'tertiary.main' }}>
+                    <ErrorOutline sx={{ fontSize: 150, mb: 4,}} />
+                    <Typography fontWeight={900} fontSize={56}>Not Found!</Typography>
+                </Box>
+            </Container>
+            <Footer />
         </>
     )
-}
+};
